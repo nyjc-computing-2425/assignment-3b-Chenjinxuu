0429b27ba2359112961e10e5969a8c007c286375
Chenjinxuu <160599422+Chenjinxuu@users.noreply.github.com>
2024-03-08 16:21:45 +0000

    Final submission
    w

--- main.py
+++ main.py
@@ -1,4 +1,13 @@
 stdform = input('Enter a number in scientific notation: ')
-stdform = stdform.strip()
+stdform = stdform.strip(" "" ")
 
 # Type your code below
+E_notation = stdform.replace("x10^", "E")
+Exponent_pos = stdform.find("^")
+Exponent = stdform[Exponent_pos+1:]
+Mantissa_pos = stdform.find("x")
+Mantissa = stdform[:Mantissa_pos]
+
+print(Exponent)
+print(Mantissa)
+print("This number is in E notation is " +  E_notation) 
